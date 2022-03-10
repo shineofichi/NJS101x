30e2d9c5f896998fab3a06e5582422f41ddb9af2
Truong Tuan Anh <anhttFX13476@funix.edu.vn>
2022-03-10 14:12:28 +0900

    LAB01-4

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
   // console.log(req);
-  res.setHeader("Content-type", "text/html");
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'></input><button>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My Node</title></head>");
   res.write("<body><h1>Hello from my Node server!</h1></body>");

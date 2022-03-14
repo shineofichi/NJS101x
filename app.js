cceda270e705dd244720c32b6c3e60085564482f
Truong Tuan Anh <anhttFX13476@funix.edu.vn>
2022-03-14 11:50:42 +0900

    LAB02-12

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -11,7 +12,7 @@ app.use(shopRoutes);
 app.use("/admin", adminRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);

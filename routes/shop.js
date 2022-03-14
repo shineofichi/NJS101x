d3b78a45ae6d552317daa9ce0b4ae374fee3a6e7
Truong Tuan Anh <anhttFX13476@funix.edu.vn>
2022-03-14 13:23:05 +0900

    LAB02-13&Styling

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
 const path = require("path");
 const express = require("express");
+const rootDir = require("../util/path");
 
 const route = express.Router();
 
 route.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = route;

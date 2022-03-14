6a793d8e27642950d6de5677244b04208b01355e
Truong Tuan Anh <anhttFX13476@funix.edu.vn>
2022-03-14 13:38:04 +0900

    LAB02-14

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ const shopRoutes = require("./routes/shop");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(shopRoutes);
 app.use("/admin", adminRoutes);
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });

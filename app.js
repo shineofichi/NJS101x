4d26d3fed40af96355cf549bdd9a9d5a5698cf5b
Truong Tuan Anh <anhttFX13476@funix.edu.vn>
2022-03-14 10:23:59 +0900

    LAB02-5

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/", (req, res, next) => {
-  console.log("App is running!");
   next();
 });
-
 app.use("/add-product", (req, res, next) => {
-  console.log("Add more middleware!");
-  res.send("<h1>This is Add product site.</h1>");
+  res.send(
+    "<html><form action='/product' method='POST'><input type='text' name='title'></input><button type='submit'>Add product</button><form></html>"
+  );
+});
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 app.use("/", (req, res, next) => {
-  console.log("Add more middleware!");
   res.send("<h1>Hello from NodeJS!</h1>");
 });
 

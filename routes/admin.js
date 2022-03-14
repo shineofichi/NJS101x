7399e33ff44eb02b54669dca8b43dad2cbd9d123
unknown <Truong Tuan Anh>
2022-03-14 22:08:10 +0900

    LAB03-1

--- routes/admin.js
+++ routes/admin.js
@@ -3,14 +3,16 @@ const express = require("express");
 const rootDir = require("../util/path");
 
 const router = express.Router();
+const product = []
 
 router.get("/add-product", (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
+  product.push({title : req.body.title})
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.product = product;

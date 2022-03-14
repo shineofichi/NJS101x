912ef8e7ad7854c0ac5d61c4127d71548b6a1cdb
Truong Tuan Anh <anhttFX13476@funix.edu.vn>
2022-03-14 11:41:59 +0900

    LAB02-11

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,10 @@
+const path = require("path");
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    "<html><form action='/admin/add-product' method='POST'><input type='text' name='title'></input><button type='submit'>Add product</button><form></html>"
-  );
+  res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
+const path = require("path");
 const express = require("express");
 
 const route = express.Router();
 
 route.get("/", (req, res, next) => {
-  res.send("<h1>Hello from NodeJS!</h1>");
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = route;

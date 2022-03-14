4182c7f08159dec4cbd29468bf751d14560dd62a
Truong Tuan Anh <anhttFX13476@funix.edu.vn>
2022-03-14 09:48:33 +0900

    LAB02-2

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("Add middleware!");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Add more middleware!");
+});
+
 const server = http.createServer(app);
-console.log(routes.someText);
+
 server.listen(3000);

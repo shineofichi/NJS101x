17e61551f66e60a84749214d614d74f64193a88f
unknown <Truong Tuan Anh>
2022-03-16 01:04:05 +0900

    LAB03-4

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(shopRoutes);
 app.use("/admin", adminData.routes);
 app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);

bc8053a0ff85d9697118d17d9f3aa69653c5f540
Truong Tuan Anh <anhttFX13476@funix.edu.vn>
2022-03-15 17:02:32 +0900

    LAB03-2

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "views");
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(shopRoutes);
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,13 @@
 const path = require("path");
 const express = require("express");
-const adminData = require("./admin")
+const adminData = require("./admin");
 const rootDir = require("../util/path");
 
 const route = express.Router();
 
 route.get("/", (req, res, next) => {
   console.log(adminData.product);
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  res.render("shop");
 });
 
 module.exports = route;

d6d086fb028e01434447a4aeae55bd8c9c613c04
unknown <Truong Tuan Anh>
2022-03-17 21:41:15 +0900

    LAB04-6

--- models/product.js
+++ models/product.js
@@ -1,6 +1,21 @@
 const fs = require("fs");
 const path = require("path");
 
+const p = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
+const getProductFromFile = (callback) =>{
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      callback([])
+    }
+    else{
+      callback(JSON.parse(fileContent));
+    }
+})
+}
 
 module.exports = class Product {
   constructor(product) {
@@ -8,35 +23,15 @@ module.exports = class Product {
   }
 
   save() {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductFromFile(products =>{
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
-      });
-    });
-  }
+    })
+  })
+}
 
   static fetchAll(callback) {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, fileContent) => {
-      if (!err) {
-        callback(JSON.parse(fileContent));
-      }
-      else{
-        callback([])
-      }
-  })}
+    getProductFromFile(callback)
+    }
 };

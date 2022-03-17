88571e13f6d6bd5071be487bba912866365f9c3e
Truong Tuan Anh <anhttFX13476@funix.edu.vn>
2022-03-17 17:01:44 +0900

    LAB04-2

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const errorController = require("./controllers/errors");
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -23,8 +24,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(shopRoutes);
 app.use("/admin", adminRoutes);
 app.use(express.static(path.join(__dirname, "public")));
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Not Found" });
-});
+app.use(errorController.get404);
 
 app.listen(3000);

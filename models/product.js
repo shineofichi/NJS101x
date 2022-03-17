171e688a9fb29be452ff3c67ef1d744d686b448c
Truong Tuan Anh <anhttFX13476@funix.edu.vn>
2022-03-17 17:35:39 +0900

    LAB04-4 part 1

--- models/product.js
+++ models/product.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 const products = [];
 
 module.exports = class Product {
@@ -7,6 +10,21 @@ module.exports = class Product {
 
   save() {
     products.push(this);
+    const p = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll() {

8617adbcb26414f659e98c04c8beae49d08126ea
unknown <Truong Tuan Anh>
2022-03-21 21:13:51 +0900

    LAB05-3

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  database: "node-complete",
+  database: "note-complete",
   password: "123123",
 });
 

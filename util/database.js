f392df5f1b1614816c26a7fac63387fe34baae06
unknown <Truong Tuan Anh>
2022-03-21 21:05:19 +0900

    LAB05-2

--- util/database.js
+++ util/database.js
@@ -1,19 +1,19 @@
-// const mysql = require("mysql2");
+const mysql = require("mysql2");
 
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   database: "node-complete",
-//   password: "123123",
-// });
-
-// module.exports = pool.promise();
-
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("node-complete", "root", "123123", {
-  dialect: "mysql",
+const pool = mysql.createPool({
   host: "localhost",
+  user: "root",
+  database: "node-complete",
+  password: "123123",
 });
 
-module.exports = sequelize;
+module.exports = pool.promise();
+
+// const Sequelize = require("sequelize");
+
+// const sequelize = new Sequelize("node-complete", "root", "123123", {
+//   dialect: "mysql",
+//   host: "localhost",
+// });
+
+// module.exports = sequelize;

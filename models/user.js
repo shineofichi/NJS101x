da9b6193f5a1ffee48a39e20177c17587290a241
unknown <Truong Tuan Anh>
2022-03-26 14:13:20 +0900

    LAB06-16

--- models/user.js
+++ models/user.js
@@ -1,9 +1,12 @@
+const { get } = require("express/lib/response");
+
 const getDb = require("../util/database").getDb;
 const ObjectId = require("mongodb").ObjectId;
 class User {
-  constructor(userName, email) {
+  constructor(userName, email, id) {
     this.name = userName;
     this.email = email;
+    this.id = id;
   }
 
   save() {
@@ -18,6 +21,17 @@ class User {
         console.log(err);
       });
   }
+  addToCart(product) {
+    const updatedProduct = { items: { ...product, quantity: 1 } };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this.id) },
+        { $set: { cart: updatedProduct } }
+      );
+  }
+
   static findById(id) {
     const db = getDb();
     return db
